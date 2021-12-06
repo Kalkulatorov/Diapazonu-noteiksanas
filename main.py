bb0a00549bdcf9485b1b97784ef5239bcbc46d32
Makson <91866536+Kalkulatorov@users.noreply.github.com>
2021-12-06 07:25:00 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+def noteiktDiapazonu(d1,d2,sk):
+    rezultats = "Skaitlis nav diapazonā!"
+    if d1>sk<=d2:
+        rezultats = "Skaitlis ir diapazonā!"
+    return rezultats
+    
+d1 = int(input("Ievadi diapazona sākumu: "))
+d2 = int(input("Ievadi diapazona beigas: "))
+sk = int(input("Ievadi skaitli: "))
+rez = noteiktDiapazonu(d1,d2,sk)
+print(rez)
